a0e6e2f5f8cd08e08cef8b480276f12fb8faf061
Urvish <ujs8t2@mail.umkc.edu>
2016-09-22 16:34:58 -0500

    Add files via upload

---
+++ CS303_Project_1/menu.cpp
@@ -0,0 +1,56 @@
+#include "menu.h"
+#include "polynomial.h"
+#include "term.h"
+
+void Menu::GetUserInput()
+{
+    string polynomial1, polynomial2;
+
+    //read first poly
+    cout << "Please enter your first polynomial: " << '\n';
+    getline(cin, polynomial1);
+
+    //read second poly
+    cout << "Please enter your second polynomial: " << '\n';
+    getline(cin, polynomial2);
+}
+
+void Menu::ShowMenu()
+{
+    cout << "********** -Polynomial Program- **********" << endl;
+    cout << "\nRules:\n";
+    cout << "You can enter your polynomial in the follwing format : ax^n + ax - 1" << endl;
+    cout << "Considering a = coefficient, x = variable and n = whole number" << endl;
+    cout << "Please do not use paranthesis." << endl;
+    cout << "You can enter either the + or - operator based on your choice for the polynomial" << endl;
+    cout << endl;
+
+    char input;
+    bool end = true;
+    while (end != false)
+    {
+        cout << "Please choose one of the following option:\n";
+        cout << "<E> to enter a Polynomial\n<N> to quit the program";
+        cout << "Please enter <E> or <N>:" << endl;
+
+        cin >> input;
+        cin.ignore(256, '\n');
+
+        switch (input)
+        {
+        case 'E':
+        case 'e':
+            GetUserInput();
+            break;
+
+        case 'N':
+        case'n':
+            end = false;
+            break;
+
+        default:
+            cout << "Please enter 'E' or 'N'" << endl;
+        }
+    }
+}
+
---
+++ CS303_Project_1/menu.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Menu
+{
+public:
+
+    Menu()
+    {
+        //no variable needs to be handeled
+    }
+
+    void ShowMenu();
+
+    void GetUserInput();
+};
