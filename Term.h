91cd3236e719a2a8c94c88433977f9c3325e9e3f
License2Learn <ambd5d@mail.umkc.edu>
2016-09-23 14:49:55 -0500

    Update Term.h

--- Term.h
+++ Term.h
@@ -15,21 +15,21 @@ private:
 
 public:
 
-	Term() {}
+	Term() {}//Default constructor
 
-	Term(int expo, int coeff);
+	Term(int expo, int coeff);//Construcotr with private variables
 
-	friend bool operator==(const Term &t1, const Term &t2);
+	friend bool operator==(const Term &t1, const Term &t2);//Checks if two terms are the same.
 
-	friend bool operator>(const Term &t1, const Term &t2);
+	friend bool operator>(const Term &t1, const Term &t2);//Checks which term has greater variables.
 
-	friend bool operator<(const Term& t1, const Term& t2);
+	friend bool operator<(const Term& t1, const Term& t2);//Checks which term has lesser vairalbes.
 
-	friend ostream& operator<<(ostream& out, const Term& m_term);
+	friend ostream& operator<<(ostream& out, const Term& m_term);//Allows terms to be output.
 
-	const Term& operator=(const Term& m_term);
+	const Term& operator=(const Term& m_term);//Assigns variables from one term to equal another.
 
-	const Term operator+(const Term& m_term);
+	const Term operator+(const Term& m_term);//Adds terms 
 
 	void ConvertTerm(string S_Term, const string& delim = "X");
 
