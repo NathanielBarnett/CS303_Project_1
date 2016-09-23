cf78e39a7d3817029f079b309625c88f8f9ffcfc
License2Learn <ambd5d@mail.umkc.edu>
2016-09-23 14:42:53 -0500

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-
 #include "polynomial.h"
 
 using namespace std;
-
+//Menu class with functions to start the program.
 class Menu
 {
 public:
+
     Menu()
     {
         //no variable needs to be handeled
     }
 
-    void ShowMenu();
+    void ShowMenu();//Prompts the user for info when called.
 
-    void GetUserInput();
+    void GetUserInput();//Allows user to input polynomials when called.
 
 private:
+
     Polynomial user_poly;
 };
