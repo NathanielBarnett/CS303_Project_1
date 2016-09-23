197cd40223164710b63819da9b330fb6a0eef760
License2Learn <ambd5d@mail.umkc.edu>
2016-09-23 15:03:23 -0500

    Update Term.cpp

--- Term.cpp
+++ Term.cpp
@@ -4,14 +4,13 @@
 #include <sstream>
 using namespace std;
 
-
 void Term::ConvertTerm(string S_Term, const string& delim)
 {
 
 	int i = 0, temp_coeff = 0, temp_expo = 0;
-	int pos = S_Term.find_first_of(delim);
-	string s_coeff = S_Term.substr(i, pos);
-	string s_expo = S_Term.substr(pos + 1);
+	int pos = S_Term.find_first_of(delim);//pos will equal the first location of a specified delim
+	string s_coeff = S_Term.substr(i, pos);//s_coeff will become the coeff variable for S_Term
+	string s_expo = S_Term.substr(pos + 1);//s_expo will become the expo variable for S_Term
 	istringstream coeff_buf(s_coeff), expo_buf(s_expo);
 	char var1, var2;
 
@@ -57,13 +56,13 @@ void Term::ConvertTerm(string S_Term, const string& delim)
 }
 
 
-Term::Term(int expo, int coeff)
+Term::Term(int expo, int coeff)//Constructor
 {
 	exponent = expo;
 	coefficient = coeff;
 }
 
-int Term::get_coeff() const
+int Term::get_coeff() const 
 {
 	return coefficient;
 }
@@ -83,33 +82,33 @@ void Term::set_expo(int expo)
 	exponent = expo;
 }
 
-bool operator==(const Term &t1, const Term &t2)
+bool operator==(const Term &t1, const Term &t2)//Returns true if they equal each other, false otherwise.
 {
 	return t1.exponent == t2.exponent;
 }
 
-bool operator>(const Term& t1, const Term& t2)
+bool operator>(const Term& t1, const Term& t2)//Returns true if t1's exponent is larger, false otherwise.
 {
 	return t1.exponent > t2.exponent;
 }
 
-bool operator<(const Term& t1, const Term& t2)
+bool operator<(const Term& t1, const Term& t2)//Returns true if t1's exponent is smaller, false otherwise.
 {
 	return t1.exponent < t2.exponent;
 }
 
-const Term& Term::operator= (const Term& m_term)
+const Term& Term::operator=(const Term& m_term)//Creates a copy of the term passed and returns it.
 {
 	this->exponent = m_term.exponent;
 	this->coefficient = m_term.coefficient;
 	return *this;
 }
 
-const Term Term::operator+(const Term& m_term)
+const Term Term::operator+(const Term& m_term)//? need help understanding, ask Urvish.
 {
 	Term temp;
 
-	if (temp.exponent = m_term.exponent)
+	if (temp.exponent == m_term.exponent)
 	{
 		temp.coefficient = this->coefficient + m_term.coefficient;
 		return temp;
@@ -118,21 +117,21 @@ const Term Term::operator+(const Term& m_term)
 		throw exception("BACON");
 }
 
-ostream& operator<<(ostream& out, const Term& m_term)
+ostream& operator<<(ostream& out, const Term& m_term)//Makes Terms printable to the screen.
 {
-	if (m_term.exponent == 0)
+	if (m_term.exponent == 0)//If Term has no exponent, return only the coeff
 	{
 		out << m_term.coefficient;
 		return out;
 	}
 
-	else if (m_term.exponent == 1)
+	else if (m_term.exponent == 1)//If Terms exponent is 1, return coeff followed by 'x'
 	{
 		out << m_term.coefficient << "x";
 		return out;
 	}
 
-	else
+	else//Return coeff + 'x' + exponent.
 	{
 		out << m_term.coefficient << "x^" << m_term.exponent;
 		return out;
