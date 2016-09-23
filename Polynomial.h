9eb00b024532f313ef4a85463965629da29a7598
Nathaniel A. Barnett <thegrtn@gmail.com>
2016-09-23 12:48:03 -0500

    Again...
    Again... 9-22-16 @ 12:47

---
+++ Polynomial.h
@@ -0,0 +1,33 @@
+/*
+Name: Group 9
+*/
+#pragma once
+#include <list>
+#include <iostream>
+#include <string>
+#include "Term.h"
+using namespace std;
+
+
+
+class Polynomial {
+
+private:
+	list<Term> L_Poly;
+
+public:
+	Polynomial() { ; }
+	// Copy Constructor
+	//Polynomial(const Polynomial& Current_Poly);
+	
+	const Polynomial operator+(const Polynomial& rhs_Poly);
+	
+	friend const ostream& operator<<(ostream& output, const Polynomial& rhs_Poly);
+	
+	//function to add Term element onto polynomial list
+	void AddTerm(string S_Term);
+
+};
+
+// Utility function for polynomial class
+void Poly_Parse(const string& s, Polynomial& Poly, const string& delim = "-+");
