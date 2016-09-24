6c598b90d2fc5f5f167caf6f82f1b9f5627f642d
License2Learn <ambd5d@mail.umkc.edu>
2016-09-24 17:33:15 -0500

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -1,6 +1,13 @@
-/*
-Name: Group 9
-*/
+//Students : Nathaniel Barnett    | Adam Bergman         | Urvish Shah
+//Email    : nabgz8@mail.umkc.edu | ambd5d@mail.umkc.edu | ujs8t2@mail.umkc.edu
+
+//Professor: Dr. Mohammad Kuhail
+//Class    : CS 303 - Data Structures 
+
+//Date     : 9/25/2016
+//Project  : Polynomial Addition Program (Project 1B)
+
+//File name:Polynomial.h
 #pragma once
 #include <list>
 #include <iostream>
